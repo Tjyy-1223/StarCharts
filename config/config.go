42ed861fe077e688e030de2fe9f7b0aae16d8386
tianjiangyu <161840237@nuaa.edu.cn>
2025-01-02 23:01:44 +0800

    config 模块

---
+++ config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v6"
+	"log"
+)
+
+// Config configuration
+type Config struct {
+	RedisURL              string   `env:"REDIS_URL" envDefault:"redis://localhost:6379"`
+	GitHubTokens          []string `env:"GITHUB_TOKENS"`
+	GitHubPageSize        int      `env:"GITHUB_PAGE_SIZE" envDefault:"100"`
+	GitHubMaxRateUsagePct int      `env:"GITHUB_MAX_RATE_LIMIT_USAGE" envDefault:"80"`
+	Listen                string   `env:"LISTEN" envDefault:"127.0.0.1:3000"`
+}
+
+func Get() (cfg Config) {
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal("failed to load config")
+	}
+	return
+}
